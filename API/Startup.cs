cdf68b3f33a650a0729195f928af303c91bef92d
goranmbre <goranmbre@gmail.com>
2021-01-19 23:38:38 +0100

    4.0

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
  
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +38,9 @@ namespace API
             app.UseHttpsRedirection();
  
             app.UseRouting();
- 
+
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+    
             app.UseAuthorization();
  
             app.UseEndpoints(endpoints =>
